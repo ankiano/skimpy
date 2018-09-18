437c9835d06ddc088d8a0329fc8a11892bc79d17
a.akinshyn <a.akinshyn@gmail.com>
2018-09-18 17:13:39 +0300

    initial commit

---
+++ setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3.6
+# coding=utf-8
+
+from pip._internal.req import parse_requirements
+from pip._internal.download import PipSession
+
+from setuptools import setup, find_packages
+
+parsed_requirements = parse_requirements(
+    'requirements.txt',
+    session=PipSession()
+)
+
+requirements = [str(ir.req) for ir in parsed_requirements]
+
+setup(
+    name='skimpy',
+    version='0.0.1',
+    description='',
+    long_description='',
+    py_modules=['skimpy'],
+    include_package_data=True,
+    install_requires=requirements,
+    zip_safe=False,
+    keywords='skimpy',
+)
---
+++ skimpy.py
@@ -0,0 +1,4 @@
+my_const = 10
+
+def my_func():
+    print('hello')
