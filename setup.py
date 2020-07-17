5bebc69c09dc669c606fb1d455f43a4f8a956a3f
ankiano <ankiano@users.noreply.github.com>
2020-07-17 22:22:43 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,14 @@
 #!/usr/bin/env python3.6
 # coding=utf-8
 
-from pip._internal.req import parse_requirements
-from pip._internal.download import PipSession
+try: # for pip >= 10
+    from pip._internal.req import parse_requirements
+except ImportError: # for pip <= 9.0.3
+    from pip.req import parse_requirements
+try: # for pip >= 10
+    from pip._internal.download import PipSession
+except ImportError: # for pip <= 9.0.3
+    from pip.download import PipSession
 
 from setuptools import setup, find_packages
 
@@ -15,7 +21,7 @@ requirements = [str(ir.req) for ir in parsed_requirements]
 
 setup(
     name='skimpy',
-    version='0.0.8',
+    version='0.0.9',
     description='',
     long_description='',
     py_modules=['skimpy'],
